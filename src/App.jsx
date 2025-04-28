fa0ec9ce0396600842d161aa1be426c49990fc6f
Sam <samuelgkeller@gmail.com>
2025-04-27 23:12:49 -0400

    Added verbs.json and implemented initial word lookup with caching

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,42 @@ import { useState } from 'react';
 
 function App() {
 
-  const [word, setWord] = useState("");
+  const [verbData, setVerbData] = useState({});
 
-  function onSubmitWord(input){
-    setWord(input);
-    console.log(word);
+  let verbCache = [];
+
+  /**
+   * Handles submission of a new verb.
+   * 
+   * Checks local cache for verb,
+   * if not found fetches from database.
+   * 
+   * @param {string} input - Japanese word entered by user (hiragana)
+   */
+  function handleWordSubmission(input){
+    if(verbCache.find(verb => verb.hiragana === input)){
+      setVerbData(verbCache.find(verb => verb.hiragana === input)); 
+    }else{
+      fetch("./verbs.json")
+        .then(response => response.json())
+        .then(values => values.forEach(value => {
+          if(value.hiragana === input){
+            const tempData = {"hiragana": value.hiragana,
+                              "kanji": value.kanji,
+                              "meaning": value.meaning,
+                              "type": value.type};
+            verbCache.push(tempData);
+            setVerbData(tempData);
+          }
+        }))
+        .catch(error => console.log(error)); // make this say input not found or something
+    }
   }
 
   return (
     <div className="mainContent">
-      <Input callback={onSubmitWord}/> {/*using callback prop get word*/}
-      <Output word={word}/> {/*send word to output as prop*/}
+      <Input callback={handleWordSubmission}/> {/*using callback prop get word*/}
+      <Output verbData={verbData}/> {/*send verb data to output as prop*/}
     </div>
   )
 }
