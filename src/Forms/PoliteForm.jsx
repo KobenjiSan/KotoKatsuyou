39d03c92bcef57b5d199197ffc1d10b0411b88ca
Sam <samuelgkeller@gmail.com>
2025-04-29 23:25:15 -0400

    Started working on info box content in polite form, added a whats happening section. Started work on .json update with past tense meanings and example sentences.

--- src/Forms/PoliteForm.jsx
+++ src/Forms/PoliteForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import styles from './Form.module.css'
 
 function PoliteForm(props){
@@ -16,20 +16,39 @@ function PoliteForm(props){
     const formName = "Polite";
     const definition = "A conjugated form of a Japanese verb used to show respect or formality, typically ending in 〜ます (masu).";
     const [isShowingBox, setIsShowingBox] = useState(false);
+    const [whatsHappening, setWhatsHappening] = useState("");
 
     function handleShowBoxToggle(){
         setIsShowingBox(!isShowingBox);
     }
 
+    useEffect(() => {
+        if(props.data.type === "ichidan"){
+            let lastCharacter = (props.data.kanji).slice(-1);
+            setWhatsHappening(`Replace ${lastCharacter} in ${props.data.kanji} with ます → `);
+        }else if(props.data.type === "godan"){
+            let lastCharacter = (props.data.kanji).slice(-1);
+            let newEnding = conversionMap[lastCharacter];
+            setWhatsHappening(`Replace ${lastCharacter} in ${props.data.kanji} with ${newEnding}, then add ます → `);
+        }else if(props.data.type === "irregular"){
+            let lastCharacters = (props.data.hiragana).slice(-2); 
+            if(lastCharacters === "する"){
+                setWhatsHappening(`Replace ${lastCharacters} in ${props.data.kanji} with します → `);
+            }else if(lastCharacters === "くる"){
+                setWhatsHappening(`Replace ${lastCharacters} in ${props.data.kanji} with きます → `);
+            }
+        }
+    }, [props.data]);
+
     function conjugateWord(){
         if(props.data.type === "ichidan"){
             const conjugatedWord = (props.data.kanji).slice(0, -1); 
-            return conjugatedWord + "ます"
+            return conjugatedWord + "ます";
         }else if(props.data.type === "godan"){
             let lastCharacter = (props.data.kanji).slice(-1);
             const conjugatedWord = (props.data.kanji).slice(0, -1); 
             let newEnding = conversionMap[lastCharacter];
-            return conjugatedWord + newEnding  + "ます"
+            return conjugatedWord + newEnding  + "ます";
         }else if(props.data.type === "irregular"){
             let lastCharacters = (props.data.hiragana).slice(-2);
             const conjugatedWord = (props.data.kanji).slice(0, -2); 
@@ -51,6 +70,8 @@ function PoliteForm(props){
             <div className={isShowingBox ? styles.infoBoxShowing : styles.infoBoxHidden}>
                 <b>{formName}</b> - {definition}
                 <br/><br/>
+                <b>What's Happening? - </b> {whatsHappening} {conjugateWord()}
+                <br /><br />
                 <b>Meaning:</b> {props.data.meaning} (politely)
                 <br/><br/>
                 <b>Example Sentence:</b> 
