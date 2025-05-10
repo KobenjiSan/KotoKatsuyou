17cf9a29fd5d51cb87bc7ac3eadc9ff6e4b98405
Sam <samuelgkeller@gmail.com>
2025-05-10 14:15:35 -0400

    Additions made to module.css files for mobile use

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -4,15 +4,17 @@ import About from "../About/About"
 
 function AboutPage(){
 
-    return(<>
-        <Link to='/' className='toLink'>
-            <h2>Home</h2>
-        </Link>
-        <div className="mainContent">
-        <Header/>
-        <About/>
-        </div>
-    </>)
+    return(
+        <>
+            <Link to='/' className='toLink'>
+                <h2>Home</h2>
+            </Link>
+            <div className="mainContent">
+            <Header/>
+            <About/>
+            </div>
+        </>
+    )
 }
 
 export default AboutPage
